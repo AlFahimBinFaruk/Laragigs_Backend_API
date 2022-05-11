7bb86673f4924d7916aba2eba746556b1acb298e
Fahim <fmd03700@gmail.com>
2022-05-11 19:41:50 +0600

    user logged in,register done using jwt-auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+     //Register user
+     Route::post("/register",[AuthController::class,'register']);
+     //Login user
+     Route::post('login', [AuthController::class,"login"]);
+     //Logout user
+     Route::post('logout', [AuthController::class,"logout"]);
+     //Get a new token 
+     Route::post('refresh', [AuthController::class,'refresh']);
+     //Get user info based on Bearer Token (*************)
+     Route::post('me', [AuthController::class,'me']);
+
 });
